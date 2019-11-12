3c418e32e84ba1ad723a85ac1bc94abc0accc183
Amrit Rathi <amritr@umich.edu>
2019-11-12 09:04:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hw4_birdwataching_amritrathi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hw4_birdwataching_amritrathi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
